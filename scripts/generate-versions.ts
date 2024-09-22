7bc60dd39db6ac5f50f70c0a6228fd8ae177eab9
Nicolas Hedger <nicolas@hedger.ch>
2024-09-22 17:40:13 +0200

    fix

--- scripts/generate-versions.ts
+++ scripts/generate-versions.ts
@@ -1,7 +1,7 @@
 import { getAllVersions } from "@biomejs/version-utils";
 import { coerce, gte } from "semver";
 
-const yankedVersions = ["1.3.2"];
+const yankedVersions: string[] = [];
 
 /**
  * Generate a list of all verions of Biome for which we want to create
@@ -10,7 +10,7 @@ const yankedVersions = ["1.3.2"];
 const versions = (await getAllVersions(false))
 	?.map((v) => coerce(v))
 	.filter((v) => v !== null)
-	.filter((v) => gte(v, "1.0.0"))
+	.filter((v) => gte(v, "1.4.0"))
 	.filter((v) => !yankedVersions.includes(v.format()))
 	.map((v) => v.format());
 

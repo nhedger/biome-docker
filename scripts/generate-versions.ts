82a3ae6403c9944d688a02ce51a630699a241128
Nicolas Hedger <nicolas@hedger.ch>
2024-09-22 17:36:58 +0200

    fix

--- scripts/generate-versions.ts
+++ scripts/generate-versions.ts
@@ -1,6 +1,8 @@
 import { getAllVersions } from "@biomejs/version-utils";
 import { coerce, gte } from "semver";
 
+const yankedVersions = ["1.3.2"];
+
 /**
  * Generate a list of all verions of Biome for which we want to create
  * Docker images.
@@ -9,6 +11,7 @@ const versions = (await getAllVersions(false))
 	?.map((v) => coerce(v))
 	.filter((v) => v !== null)
 	.filter((v) => gte(v, "1.0.0"))
+	.filter((v) => !yankedVersions.includes(v.format()))
 	.map((v) => v.format());
 
 console.log(JSON.stringify(versions));
